e806b565b96b205d5a969cd6b9e2f63c6f3d8249
Nathan Heim <128533050+Nate-Heim@users.noreply.github.com>
2023-12-14 16:03:33 -0700

    update

--- Assignment-14/js/dragon.js
+++ Assignment-14/js/dragon.js
@@ -2,46 +2,54 @@ document.addEventListener('DOMContentLoaded', function () {
     var canvas = document.getElementById('dragonCanvas');
     var context = canvas.getContext('2d');
 
-    // Drawing the dragons body here
-    context.fillStyle = 'green';
-    context.fillRect(100, 150, 200, 100);
+    //body
+    context.fillStyle = '#00796b';
+    context.fillRect(120, 180, 160, 100);
 
-    // Drawing the dragons head
+    //head
     context.beginPath();
-    context.arc(300, 200, 30, 0, 2 * Math.PI);
-    context.fillStyle = 'green';
+    context.arc(200, 150, 40, 0, 2 * Math.PI);
+    context.fillStyle = '#00796b';
     context.fill();
     context.stroke();
 
-    // Drawing the dragons tail
+    //eyes
     context.beginPath();
-    context.moveTo(100, 200);
-    context.lineTo(70, 180);
-    context.lineTo(70, 220);
-    context.closePath();
-    context.fillStyle = 'green';
+    context.arc(190, 140, 5, 0, 2 * Math.PI);
+    context.arc(210, 140, 5, 0, 2 * Math.PI);
+    context.fillStyle = 'white';
     context.fill();
-    context.stroke();
 
-    // Drawing the dragons wings
+    // pupils
     context.beginPath();
-    context.moveTo(200, 100);
-    context.lineTo(150, 120);
-    context.lineTo(150, 80);
-    context.closePath();
-    context.fillStyle = 'green';
-    context.fill();
-    context.stroke();
-
-    // Drawing the dragons eyes
-    context.beginPath();
-    context.arc(310, 190, 5, 0, 2 * Math.PI);
+    context.arc(190, 140, 2, 0, 2 * Math.PI);
+    context.arc(210, 140, 2, 0, 2 * Math.PI);
     context.fillStyle = 'black';
     context.fill();
 
-    // Drawing the dragons mouth
+    //mouth
     context.beginPath();
-    context.arc(310, 210, 10, 0.8 * Math.PI, 1.2 * Math.PI);
+    context.arc(200, 155, 15, 0.2 * Math.PI, 0.8 * Math.PI);
     context.lineWidth = 2;
+    context.stroke();
+
+    // wings
+    context.beginPath();
+    context.moveTo(120, 150);
+    context.lineTo(70, 100);
+    context.lineTo(70, 200);
+    context.closePath();
+    context.fillStyle = '#00796b';
+    context.fill();
+    context.stroke();
+
+    // tail
+    context.beginPath();
+    context.moveTo(120, 230);
+    context.lineTo(70, 250);
+    context.lineTo(70, 210);
+    context.closePath();
+    context.fillStyle = '#00796b';
+    context.fill();
     context.stroke();
 });
